cb812e941c9d5b9fe2554cd631c3bb3230fd0eaa
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-05-12 23:29:05 +0600

    adds mario challenge

--- challenge/mario.c
+++ challenge/mario.c
@@ -2,14 +2,43 @@
 #include "mylibc/utils.h"
 int main(void)
 {
-    int height = 0;
 
-    // printf("Enter the height: ");
-    // int user_input = scanf("%d", &height);
+    int HEIGHT = 4;
 
-    int x = read_valid_int("Enter a valid positive number: ");
+    // How to put that space?
+    // how to add hash gradually?
 
-    printf("you have entered: %d\n", x);
+    // Algorithm
+
+    for (int i = 0; i < HEIGHT; i++)
+    {
+
+        //  Right aligned pyramid
+        for (int j = 0; j < ((HEIGHT - i) - 1); j++)
+        {
+            // printf("loop: %d, ", j);
+            // printf("%i -> %i\n", (HEIGHT - i), ((HEIGHT - i) - 1));
+
+            // printf("space");
+            printf(" ");
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        // left aligned pyramid
+        printf(" ");
+
+        for (int x = 0; x <= i; x++)
+        {
+            /* code */
+            printf("#");
+        }
+
+        printf("\n");
+    }
 
     return 0;
 }
